927036d557fd288c89b61838054dd236f5ad6c46
otomad <56647156+otomad@users.noreply.github.com>
2022-09-08 08:07:20 +0800

    Update

--- src/settings/Setting.ts
+++ src/settings/Setting.ts
@@ -1,6 +1,6 @@
 // 取名为 Setting 而不是 Settings 以免和内置对象冲突。
 
-import hasOwn from "../module/hasOwn";
+import hasOwn from "../modules/hasOwn";
 import SettingsHelper from "./SettingsHelper";
 
 const defs = {
@@ -16,21 +16,19 @@ const defs = {
 };
 
 type SettingTag = keyof typeof defs;
-type GetterSettings<T extends SettingTag> = `get${T}`;
-type SetterSettings<T extends SettingTag> = `set${T}`;
-type SettingValue<T> = typeof defs[T extends GetterSettings<infer P> ? P : T extends SetterSettings<infer Q> ? Q : never];
+type SettingValue<T extends SettingTag> = typeof defs[T];
 type SettingValues = typeof defs[SettingTag];
 
-type SettingType<T extends SettingTag> =
-	{ [getter in GetterSettings<T>]: (def?: SettingValue<getter>) => SettingValue<getter> } &
-	{ [setter in SetterSettings<T>]: (value: SettingValue<setter>) => void };
-const Setting = {} as SettingType<SettingTag>;
-for (const tag in defs) {
+type SettingsType=
+	{ [tag in SettingTag as `get${tag}`]: (def?: SettingValue<tag>) => SettingValue<tag> } &
+	{ [tag in SettingTag as `set${tag}`]: (value: SettingValue<tag>) => void };
+
+const Setting = {} as SettingsType;
+for (const tag in defs)
 	if (hasOwn(defs, tag)) {
-		type _GetLoose = { [getter in GetterSettings<SettingTag>]: (def?: SettingValues) => SettingValues };
+		type _GetLoose = { [tag in SettingTag as `get${tag}`]: (def?: SettingValues) => SettingValues };
 		(Setting as _GetLoose)[`get${tag}`] = (def = defs[tag]) => SettingsHelper.get(tag, def);
 		Setting[`set${tag}`] = value => SettingsHelper.set(tag, value);
 	}
-}
 
 export default Setting;
