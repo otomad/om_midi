966175d83355d8466bf5a36b8da3540af142f17c
otomad <56647156+otomad@users.noreply.github.com>
2022-10-23 14:45:12 +0800

    Update

--- install.bat
+++ install.bat
@@ -1,1 +1,31 @@
+@echo off
+:: utf-8
+chcp >nul 2>nul 65001
 
+:: check admin
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+	echo 请以管理员身份运行安装程序。
+	echo Please run install as an administrator.
+	goto getAdmin
+) else goto skipAdmin
+:getAdmin
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+:skipAdmin
+
+:: prompt
+echo 即将安装 om midi for After Effects 脚本，按回车键继续。
+echo The om midi for After Effects script will be installed soon, press Enter to continue.
+set /p input=
+cls
+
+:: add regs
+for /l %%i in (4,1,11) do (
+	reg >nul 2>nul add HKEY_CURRENT_USER\Software\Adobe\CSXS.%%i /v PlayerDebugMode /t REG_SZ /d "1" /f
+)
+reg >nul 2>nul add HKEY_CURRENT_USER\Software\Adobe\CSXS.6.1 /v PlayerDebugMode /t REG_SZ /d "1" /f
+
+:: end
+echo 安装完成！
+echo Installation complete!
+pause
