3886b50f78bdd6a74cbc1a68b299fbfd265b75d4
otomad <shuishenme@outlook.com>
2023-10-17 21:50:39 +0800

    Try vite + react

--- src/modules/getPath.ts
+++
@@ -1,9 +0,0 @@
-export default function getPath(target: unknown): Element[] {
-	if (!(target instanceof Element)) return [];
-	const path: Element[] = [];
-	while (target instanceof Element) {
-		path.push(target);
-		target = target.parentElement;
-	}
-	return path;
-}
--- src/modules/props.ts
+++
@@ -1,5 +0,0 @@
-export type Children = React.HTMLProps<HTMLElement>["children"];
-
-export interface ChildrenProps {
-	children?: Children;
-}
